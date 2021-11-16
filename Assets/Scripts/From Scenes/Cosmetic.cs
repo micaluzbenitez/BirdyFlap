c7f16cbe262f0f771cc3e4b9323dcb65eb4a6a44
Guido Tello <lpmaleinn@gmail.com>
2021-11-16 20:29:31 -0300

    Added Cosmetic Class
    To control skins

---
+++ Assets/Scripts/From Scenes/Cosmetic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CosmeticType
+{
+    Hat,
+    Beak,
+    Eyes
+}
+public struct Price
+{
+    public int quantity;
+    public CurrencyType currencyType;
+}
+
+[System.Serializable]
+public class Cosmetic
+{
+    public CosmeticType cosmetic;
+    [SerializeField] private Sprite sprite;
+    [SerializeField] private Price price;
+
+    private bool bought = false;
+
+    public void Buy() => bought = true;
+    public bool IsBought() { return bought; }    
+    public Price GetPrice() { return price; }    
+    
+}
