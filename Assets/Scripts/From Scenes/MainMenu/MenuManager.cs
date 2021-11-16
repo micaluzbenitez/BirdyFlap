9413c259193078e3a63c739e5160deb83134c850
Guido Tello <lpmaleinn@gmail.com>
2021-11-16 20:26:40 -0300

    Added MainMenu Scene

---
+++ Assets/Scripts/From Scenes/MainMenu/MenuManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    [SerializeField] private CanvasGroup mainMenuPanel;
+    [SerializeField] private CanvasGroup creditPanel;
+
+    public float showSpeed = 1;
+
+    public void Start()
+    {
+        LoadMainMenuPanel();
+    }
+    public void LoadMainMenuPanel()
+    {
+        StopAllCoroutines();
+        UnloadPanel(creditPanel);
+        LoadPanel(mainMenuPanel);
+    }
+
+    public void LoadCreditsPanel()
+    {
+        StopAllCoroutines();
+        UnloadPanel(mainMenuPanel);
+        LoadPanel(creditPanel);
+    }
+    public void LoadStoreScene()
+    {
+        SceneManager.LoadScene("Store");
+    }
+
+    private void UnloadPanel(CanvasGroup panel)
+    {
+        StartCoroutine(UnloadPanelCoroutine(panel));
+    }
+    private void LoadPanel(CanvasGroup panel)
+    {
+        StartCoroutine(LoadPanelCoroutine(panel));
+    }
+
+    IEnumerator UnloadPanelCoroutine(CanvasGroup panel)
+    {
+        if (panel.alpha > 0)
+        {
+            float t = 1;
+            while (t > 0)
+            {
+                panel.alpha = Mathf.Lerp(0, 1, t);
+                t -= Time.deltaTime * showSpeed;
+                yield return null;
+            }
+            panel.alpha = 0;
+            panel.interactable = false;
+            panel.blocksRaycasts = false;
+        }
+    }
+
+    IEnumerator LoadPanelCoroutine(CanvasGroup panel)
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            panel.alpha = Mathf.Lerp(0, 1, t);
+            t += Time.deltaTime * showSpeed;
+            yield return null;
+        }
+        panel.alpha = 1;
+        panel.interactable = true;
+        panel.blocksRaycasts = true;
+    }
+
+    public void LoadGame(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
